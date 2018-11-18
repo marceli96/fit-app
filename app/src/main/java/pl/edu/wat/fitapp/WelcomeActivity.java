ccf9e0ce7e2034a8fe17fb970888ef9700f49ec3
Marcel <marcel.fituch@student.wat.edu.pl>
2018-11-18 19:20:20 +0100

    Add buttons functionality in Welcome Screen

--- app/src/main/java/pl/edu/wat/fitapp/WelcomeActivity.java
+++ app/src/main/java/pl/edu/wat/fitapp/WelcomeActivity.java
@@ -1,13 +1,37 @@
 package pl.edu.wat.fitapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button bLogin, bRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button bLogin = (Button)findViewById(R.id.bLogin);
+        Button bRegsiter = (Button)findViewById(R.id.bRegister);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openLoginScreen = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(openLoginScreen);
+            }
+        });
+
+        bRegsiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openRegisterScreen = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(openRegisterScreen);
+            }
+        });
+
     }
 }
