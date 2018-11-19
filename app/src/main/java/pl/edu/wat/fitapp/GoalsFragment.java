71175f92a0577937507aa0a3bae29ecb452717ee
marceli96 <marcel.fituch@student.wat.edu.pl>
2018-11-19 22:42:46 +0100

    Add Screens: About, Export, Goals, Progress, Recipes, Settings

---
+++ app/src/main/java/pl/edu/wat/fitapp/GoalsFragment.java
@@ -0,0 +1,29 @@
+package pl.edu.wat.fitapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GoalsFragment extends Fragment {
+
+
+    public GoalsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_goals, container, false);
+    }
+
+}
