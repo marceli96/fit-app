90dfdc71313ad156c021e3107730b04dabcbd428
marceli96 <marcel.fituch@student.wat.edu.pl>
2018-11-19 02:59:48 +0100

    Add Nagivation Drawer GUI to Main Screen

--- app/src/main/java/pl/edu/wat/fitapp/MainActivity.java
+++ app/src/main/java/pl/edu/wat/fitapp/MainActivity.java
@@ -4,8 +4,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -15,11 +20,17 @@ public class MainActivity extends AppCompatActivity {
     private HomeFragment homeFragment;
     private JournalFragment journalFragment;
     private MeFragment meFragment;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         mainNavigation = (BottomNavigationView)findViewById(R.id.mainNavigation);
         mainFrame = (FrameLayout)findViewById(R.id.mainFrame);
@@ -27,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         homeFragment = new HomeFragment();
         journalFragment = new JournalFragment();
         meFragment = new MeFragment();
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         setFragment(homeFragment);
 
@@ -48,6 +66,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 
     private void setFragment(Fragment fragment) {
