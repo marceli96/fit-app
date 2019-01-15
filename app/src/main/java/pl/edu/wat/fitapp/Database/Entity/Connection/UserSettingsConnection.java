a087a45e004fe705acdee976cdd037f611d4d98f
marceli96 <marcel.fituch@student.wat.edu.pl>
2019-01-15 23:48:34 +0100

    Split connection to database for SettingsFragment

---
+++ app/src/main/java/pl/edu/wat/fitapp/Database/Entity/Connection/UserSettingsConnection.java
@@ -0,0 +1,161 @@
+package pl.edu.wat.fitapp.Database.Entity.Connection;
+
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.edu.wat.fitapp.Database.Entity.User;
+import pl.edu.wat.fitapp.Main.Fragment.SettingsFragment;
+
+public class UserSettingsConnection {
+    private final String OPERATIONS_URL = "http://fitappliaction.cba.pl/operations.php";
+    private SettingsFragment settingsFragment;
+
+    public UserSettingsConnection(SettingsFragment settingsFragment) {
+        this.settingsFragment = settingsFragment;
+    }
+
+    public void changeLogin(final User user, final String newUserName) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean available = jsonResponse.getBoolean("available");
+                            if (available) {
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    user.setUserName(newUserName);
+                                    settingsFragment.setUser(user);
+                                    Toast.makeText(settingsFragment.getActivity(), "Login zmieniony pomyślnie", Toast.LENGTH_LONG).show();
+                                    settingsFragment.openHomeActivity();
+                                } else
+                                    Toast.makeText(settingsFragment.getActivity(), "Nieoczekiwany błąd", Toast.LENGTH_LONG).show();
+                            } else
+                                Toast.makeText(settingsFragment.getActivity(), "Nazwa użytkownika jest zajęta", Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(settingsFragment.getActivity(), "Settings error! " + e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(settingsFragment.getActivity(), "Settings error! " + error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("operation", "setUsername");
+                params.put("userId", String.valueOf(user.getUserID()));
+                params.put("userName", newUserName);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(settingsFragment.getActivity());
+        requestQueue.add(stringRequest);
+
+    }
+
+    public void changeEmail(final User user, final String newEmail) {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean available = jsonResponse.getBoolean("available");
+                                if (available) {
+                                    boolean success = jsonResponse.getBoolean("success");
+                                    if (success) {
+                                        user.setEmail(newEmail);
+                                        settingsFragment.setUser(user);
+                                        Toast.makeText(settingsFragment.getActivity(), "E-mail zmieniony pomyślnie", Toast.LENGTH_LONG).show();
+                                        settingsFragment.openHomeActivity();
+                                    } else
+                                        Toast.makeText(settingsFragment.getActivity(), "Nieoczekiwany błąd", Toast.LENGTH_LONG).show();
+                                } else
+                                    Toast.makeText(settingsFragment.getActivity(), "E-mail jest zajęty", Toast.LENGTH_LONG).show();
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(settingsFragment.getActivity(), "Settings error! " + e.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(settingsFragment.getActivity(), "Settings error! " + error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("operation", "setEmail");
+                    params.put("userId", String.valueOf(user.getUserID()));
+                    params.put("email", newEmail);
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(settingsFragment.getActivity());
+            requestQueue.add(stringRequest);
+    }
+
+    public void changePassword(final User user, final String newPassword) {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    Toast.makeText(settingsFragment.getActivity(), "Hasło zmienione pomyślnie", Toast.LENGTH_LONG).show();
+                                    settingsFragment.openHomeActivity();
+                                } else
+                                    Toast.makeText(settingsFragment.getActivity(), "Nieoczekiwany błąd", Toast.LENGTH_LONG).show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(settingsFragment.getActivity(), "Settings error! " + e.toString(), Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(settingsFragment.getActivity(), "Settings error! " + error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("operation", "setPassword");
+                    params.put("userId", String.valueOf(user.getUserID()));
+                    params.put("password", newPassword);
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(settingsFragment.getActivity());
+            requestQueue.add(stringRequest);
+    }
+}
--- app/src/main/java/pl/edu/wat/fitapp/Main/Fragment/SettingsFragment.java
+++ app/src/main/java/pl/edu/wat/fitapp/Main/Fragment/SettingsFragment.java
@@ -25,6 +25,7 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import pl.edu.wat.fitapp.Database.Entity.Connection.UserSettingsConnection;
 import pl.edu.wat.fitapp.Database.Entity.User;
 import pl.edu.wat.fitapp.Main.MainActivity;
 import pl.edu.wat.fitapp.R;
@@ -32,14 +33,13 @@ import pl.edu.wat.fitapp.R;
 
 public class SettingsFragment extends Fragment {
 
-    private final String OPERATIONS_URL = "http://fitappliaction.cba.pl/operations.php";
-
     private EditText etLogin, etEmail, etPassword1, etPassword2;
     private Button bChangeLogin, bChangeEmail, bChangePassword;
     private TextView tvLogin, tvEmail;
 
     private User user;
 
+    private UserSettingsConnection userSettingsConnection;
 
     public SettingsFragment() {
     }
@@ -70,28 +70,47 @@ public class SettingsFragment extends Fragment {
         bChangePassword = view.findViewById(R.id.bChangePassword);
 
         bChangeLogin.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                changeLogin();
+                if (!etLogin.getText().toString().isEmpty() && etLogin.getText().toString().length() > 5) {
+                    userSettingsConnection = new UserSettingsConnection(SettingsFragment.this);
+                    userSettingsConnection.changeLogin(user, etLogin.getText().toString());
+                } else {
+                    if (etLogin.getText().toString().isEmpty())
+                        Toast.makeText(getActivity(), "Wprowadź login", Toast.LENGTH_SHORT).show();
+                    else if (etLogin.getText().toString().length() <= 5)
+                        Toast.makeText(getActivity(), "Login musi mieć conajmniej 6 znaków", Toast.LENGTH_SHORT).show();
+                }
             }
-
         });
 
         bChangeEmail.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                changeEmail();
+                if (!etEmail.getText().toString().isEmpty()) {
+                    userSettingsConnection = new UserSettingsConnection(SettingsFragment.this);
+                    userSettingsConnection.changeEmail(user, etEmail.getText().toString());
+                } else {
+                    Toast.makeText(getActivity(), "Wprowadź e-mail", Toast.LENGTH_SHORT).show();
+                }
             }
-
         });
 
         bChangePassword.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                changePassword();
+                if (!etPassword1.getText().toString().isEmpty() && etPassword1.getText().toString().length() > 5
+                        && etPassword1.getText().toString().equals(etPassword2.getText().toString())) {
+                    userSettingsConnection = new UserSettingsConnection(SettingsFragment.this);
+                    userSettingsConnection.changePassword(user, etPassword1.getText().toString());
+                } else {
+                    if (etPassword1.getText().toString().isEmpty())
+                        Toast.makeText(getActivity(), "Wprowadź hasło", Toast.LENGTH_SHORT).show();
+                    else if (!etPassword1.getText().toString().equals(etPassword2.getText().toString()))
+                        Toast.makeText(getActivity(), "Hasła się różnią", Toast.LENGTH_SHORT).show();
+                    else if (etPassword1.getText().toString().length() <= 5)
+                        Toast.makeText(getActivity(), "Hasło musi mieć conajmniej 6 znaków", Toast.LENGTH_SHORT).show();
+                }
             }
 
         });
@@ -99,171 +118,14 @@ public class SettingsFragment extends Fragment {
         return view;
     }
 
-
-    public void changeLogin() {
-        if (!etLogin.getText().toString().isEmpty() && etLogin.getText().toString().length() > 5) {
-            final String userName = etLogin.getText().toString();
-
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject jsonResponse = new JSONObject(response);
-                                boolean available = jsonResponse.getBoolean("available");
-                                if (available) {
-                                    boolean success = jsonResponse.getBoolean("success");
-                                    if (success) {
-                                        user.setUserName(userName);
-                                        Toast.makeText(getActivity(), "Login zmieniony pomyślnie", Toast.LENGTH_LONG).show();
-                                        openHomeActivity();
-                                    } else
-                                        Toast.makeText(getActivity(), "Nieoczekiwany błąd", Toast.LENGTH_LONG).show();
-                                } else
-                                    Toast.makeText(getActivity(), "Nazwa użytkownika jest zajęta", Toast.LENGTH_LONG).show();
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(getActivity(), "Settings error! " + e.toString(), Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getActivity(), "Settings error! " + error.toString(),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("operation", "setUsername");
-                    params.put("userId", String.valueOf(user.getUserID()));
-                    params.put("userName", userName);
-                    return params;
-                }
-            };
-
-            RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-            requestQueue.add(stringRequest);
-        } else {
-            if (etLogin.getText().toString().isEmpty())
-                Toast.makeText(getActivity(), "Wprowadź login", Toast.LENGTH_SHORT).show();
-            else if (etLogin.getText().toString().length() <= 5)
-                Toast.makeText(getActivity(), "Login musi mieć conajmniej 6 znaków", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void changeEmail() {
-        if (!etEmail.getText().toString().isEmpty()) {
-            final String email = etEmail.getText().toString();
-
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject jsonResponse = new JSONObject(response);
-                                boolean available = jsonResponse.getBoolean("available");
-                                if (available) {
-                                    boolean success = jsonResponse.getBoolean("success");
-                                    if (success) {
-                                        user.setEmail(email);
-                                        Toast.makeText(getActivity(), "E-mail zmieniony pomyślnie", Toast.LENGTH_LONG).show();
-                                        openHomeActivity();
-                                    } else
-                                        Toast.makeText(getActivity(), "Nieoczekiwany błąd", Toast.LENGTH_LONG).show();
-                                } else
-                                    Toast.makeText(getActivity(), "E-mail jest zajęty", Toast.LENGTH_LONG).show();
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(getActivity(), "Settings error! " + e.toString(), Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getActivity(), "Settings error! " + error.toString(),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("operation", "setEmail");
-                    params.put("userId", String.valueOf(user.getUserID()));
-                    params.put("email", email);
-                    return params;
-                }
-            };
-
-            RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-            requestQueue.add(stringRequest);
-        } else {
-            Toast.makeText(getActivity(), "Wprowadź e-mail", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void changePassword() {
-        if (!etPassword1.getText().toString().isEmpty() && etPassword1.getText().toString().length() > 5
-                && etPassword1.getText().toString().equals(etPassword2.getText().toString())) {
-            final String password = etPassword1.getText().toString();
-
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject jsonResponse = new JSONObject(response);
-                                boolean success = jsonResponse.getBoolean("success");
-                                if (success) {
-                                    Toast.makeText(getActivity(), "Hasło zmienione pomyślnie", Toast.LENGTH_LONG).show();
-                                    openHomeActivity();
-                                } else
-                                    Toast.makeText(getActivity(), "Nieoczekiwany błąd", Toast.LENGTH_LONG).show();
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(getActivity(), "Settings error! " + e.toString(), Toast.LENGTH_LONG).show();
-                            }
-
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getActivity(), "Settings error! " + error.toString(), Toast.LENGTH_LONG).show();
-                        }
-                    }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("operation", "setPassword");
-                    params.put("password", password);
-                    params.put("userId", String.valueOf(user.getUserID()));
-                    return params;
-                }
-            };
-
-            RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-            requestQueue.add(stringRequest);
-
-        } else {
-            if (etPassword1.getText().toString().isEmpty())
-                Toast.makeText(getActivity(), "Wprowadź hasło", Toast.LENGTH_SHORT).show();
-            else if (!etPassword1.getText().toString().equals(etPassword2.getText().toString()))
-                Toast.makeText(getActivity(), "Hasła się różnią", Toast.LENGTH_SHORT).show();
-            else if (etPassword1.getText().toString().length() <= 5)
-                Toast.makeText(getActivity(), "Hasło musi mieć conajmniej 6 znaków", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     public void openHomeActivity() {
         Intent openHomeScreen = new Intent(getActivity(), MainActivity.class);
         openHomeScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         openHomeScreen.putExtra("user", user);
         startActivity(openHomeScreen);
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
