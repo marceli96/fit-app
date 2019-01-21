8d719fd2aa706ca03dbb49579b610266635236c0
marceli96 <marcel.fituch@student.wat.edu.pl>
2019-01-21 18:21:11 +0100

    Split database connection for AddIngredintToFoodSystemFragment

--- app/src/main/java/pl/edu/wat/fitapp/Main/Fragment/AddToSystem/AddIngredientToFoodSystemFragment.java
+++ app/src/main/java/pl/edu/wat/fitapp/Main/Fragment/AddToSystem/AddIngredientToFoodSystemFragment.java
@@ -2,17 +2,13 @@ package pl.edu.wat.fitapp.Main.Fragment.AddToSystem;
 
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -29,15 +25,17 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import pl.edu.wat.fitapp.AndroidComponent.ListAdapter.IngredientsListAdapter;
+import pl.edu.wat.fitapp.Database.Connection.IngredientsConnection;
 import pl.edu.wat.fitapp.Database.Entity.Ingredient;
 import pl.edu.wat.fitapp.Database.Entity.User;
+import pl.edu.wat.fitapp.Dialog.AddIngredientToFoodSystemOnClickDialog;
 import pl.edu.wat.fitapp.Main.MainActivity;
 import pl.edu.wat.fitapp.R;
 
@@ -48,7 +46,9 @@ public class AddIngredientToFoodSystemFragment extends Fragment {
 
     private ListView lvIngredients;
     private ArrayList<Ingredient> ingredientList;
-    private IngredientListAdapter ingredientListAdapter;
+    private IngredientsListAdapter ingredientsListAdapter;
+
+    private IngredientsConnection ingredientsConnection;
 
     private User user;
     private int mealTime;
@@ -59,181 +59,32 @@ public class AddIngredientToFoodSystemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_ingredient_to_food_system, container, false);
+
         user = (User) getActivity().getIntent().getSerializableExtra("user");
         mealTime = (int) getActivity().getIntent().getSerializableExtra("mealTime");
 
         ingredientList = new ArrayList<>();
-        getIngredients();
-
-        View view = inflater.inflate(R.layout.fragment_add_ingredient_to_food_system, container, false);
 
         lvIngredients = view.findViewById(R.id.lvIngredients);
-        ingredientListAdapter = new IngredientListAdapter(getActivity(), R.layout.listview_adapter_add_ingredient, ingredientList);
-        lvIngredients.setAdapter(ingredientListAdapter);
+        ingredientsListAdapter = new IngredientsListAdapter(getActivity(), R.layout.listview_adapter_add_ingredient, ingredientList);
+        lvIngredients.setAdapter(ingredientsListAdapter);
+
+        ingredientsConnection = new IngredientsConnection(getActivity(), ingredientList);
+        ingredientsConnection.getIngredients(ingredientsListAdapter);
 
         lvIngredients.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                final View alertView = getLayoutInflater().inflate(R.layout.dialog_weight_choose_inredient, null);
-
-                TextView tvIngredientName = alertView.findViewById(R.id.tvIngredientName);
-                Button bAddIngredientToFoodSystem = alertView.findViewById(R.id.bAddIngredientToFoodSystem);
-
-                tvIngredientName.setText(ingredientList.get(position).getName());
-
-                builder.setView(alertView);
-                final AlertDialog dialog = builder.create();
-                dialog.show();
-
-                bAddIngredientToFoodSystem.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        EditText etWeight = alertView.findViewById(R.id.etWeight);
-                        String weight = etWeight.getText().toString();
-                        if (weight.isEmpty())
-                            Toast.makeText(getActivity(), "Wpisz wagę!", Toast.LENGTH_SHORT).show();
-                        else {
-                            addIngredientToFoodSystem(ingredientList.get(position).getID(), user.getUserID(), mealTime, weight);
-                            dialog.dismiss();
-                        }
-                    }
-                });
+                AddIngredientToFoodSystemOnClickDialog addIngredientToFoodSystemOnClickDialog = new AddIngredientToFoodSystemOnClickDialog(AddIngredientToFoodSystemFragment.this);
+                addIngredientToFoodSystemOnClickDialog.build(position, ingredientList, user.getUserID(), mealTime);
             }
         });
 
         return view;
     }
 
-    class IngredientListAdapter extends ArrayAdapter<Ingredient> {
-
-        public IngredientListAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Ingredient> objects) {
-            super(context, resource, objects);
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.listview_adapter_add_ingredient, parent, false);
-
-            TextView tvIngredientName = convertView.findViewById(R.id.tvIngredientName);
-            TextView tvIngredientCarbohydrates = convertView.findViewById(R.id.tvIngredientCarbohydrates);
-            TextView tvIngredientProtein = convertView.findViewById(R.id.tvIngredientProtein);
-            TextView tvIngredientFat = convertView.findViewById(R.id.tvIngredientFat);
-            TextView tvIngredientCalories = convertView.findViewById(R.id.tvIngredientCalories);
-
-            DecimalFormat decimalFormat = new DecimalFormat("0.0");
-
-            String temp;
-            tvIngredientName.setText(ingredientList.get(position).getName());
-
-            temp = String.valueOf(decimalFormat.format(ingredientList.get(position).getCarbohydrates())) + " g";
-            tvIngredientCarbohydrates.setText(temp);
-
-            temp = String.valueOf(decimalFormat.format(ingredientList.get(position).getProtein())) + " g";
-            tvIngredientProtein.setText(temp);
-
-            temp = String.valueOf(decimalFormat.format(ingredientList.get(position).getFat())) + " g";
-            tvIngredientFat.setText(temp);
-
-            temp = String.valueOf(decimalFormat.format(ingredientList.get(position).getCalories())) + " kcal";
-            tvIngredientCalories.setText(temp);
-
-            return convertView;
-        }
-    }
-
-    private void getIngredients() {
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    boolean success = jsonResponse.getBoolean("success");
-                    if (success) {
-                        for (int i = 0; i < jsonResponse.length() - 1; i++) {
-                            JSONObject ingredient = jsonResponse.getJSONObject(String.valueOf(i));
-                            ingredientList.add(new Ingredient(ingredient.getInt("ID_Ingredient"), ingredient.getString("IngredientName"),
-                                    ingredient.getDouble("Carbohydrates"), ingredient.getDouble("Protein"), ingredient.getDouble("Fat"),
-                                    ingredient.getInt("Calories")));
-                        }
-                        ingredientListAdapter.notifyDataSetChanged();
-                    } else
-                        Toast.makeText(getContext(), "Błąd połączenia z bazą", Toast.LENGTH_SHORT).show();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getContext(), "Błąd połączenia z bazą " + e.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getContext(), "Błąd połączenia z bazą " + error.toString(), Toast.LENGTH_SHORT).show();
-
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("operation", "getIngredients");
-                return params;
-            }
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
-        requestQueue.add(stringRequest);
-    }
-
-    private void addIngredientToFoodSystem(final int ingredientId, final int userID, final int mealTime, final String weight) {
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, OPERATIONS_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    boolean message = jsonResponse.getBoolean("message");
-                    if (!message) {
-                        Toast.makeText(getActivity(), "Dany składnik został już dodany w tej porze jedzenia", Toast.LENGTH_SHORT).show();
-                    } else {
-                        boolean success = jsonResponse.getBoolean("success");
-                        if (success) {
-                            Toast.makeText(getActivity(), "Dodano pomyślnie", Toast.LENGTH_SHORT).show();
-                            openMainActivity();
-                        } else {
-                            Toast.makeText(getActivity(), "Błąd podczas dodawania", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getActivity(), "Błąd podczas dodawania " + e.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getActivity(), "Błąd podczas dodawania " + error.toString(), Toast.LENGTH_SHORT).show();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                Date date = new Date();
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                params.put("operation", "addIngredientToFoodSystem");
-                params.put("ingredientId", String.valueOf(ingredientId));
-                params.put("userId", String.valueOf(userID));
-                params.put("mealTime", String.valueOf(mealTime));
-                params.put("weight", weight);
-                params.put("date", dateFormat.format(date));
-                return params;
-            }
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-        requestQueue.add(stringRequest);
-    }
-
-    private void openMainActivity() {
+    public void openMainActivity() {
         Intent openMainActivity = new Intent(getContext(), MainActivity.class);
         openMainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         openMainActivity.putExtra("user", user);
